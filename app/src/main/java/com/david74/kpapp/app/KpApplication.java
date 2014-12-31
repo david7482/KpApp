312481a64780fadfdb4e57663e0e2414f5782351
david_t_chou <david_t_chou@trend.com.tw>
2014-12-31 18:09:16 +0800

    Add default display option to UIL

--- app/src/main/java/com/david74/kpapp/app/KpApplication.java
+++ app/src/main/java/com/david74/kpapp/app/KpApplication.java
@@ -2,6 +2,7 @@ package com.david74.kpapp.app;
 
 import android.app.Application;
 
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
@@ -9,9 +10,21 @@ public class KpApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        initUniversalImageLoader();
+    }
 
-        // Create global configuration and initialize ImageLoader with this config
-        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this).build();
+    // Create global configuration and initialize ImageLoader with this config
+    void initUniversalImageLoader() {
+        DisplayImageOptions options = new DisplayImageOptions.Builder()
+                .showImageOnLoading(android.R.color.transparent)
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this)
+                .defaultDisplayImageOptions(options)
+                .build();
+
         ImageLoader.getInstance().init(config);
     }
 }
